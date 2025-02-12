4c0e12b9c7981139bff6613b6cf87da4b1e736c3
Pratikpatil43 <patilpratik4388@gmail.com>
2025-02-12 15:25:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   const [rating, setRating] = useState(0);
@@ -45,6 +46,7 @@ function App() {
 
   return (
     <div className="App">
+      <Analytics/>
       {/* Hero Section */}
       <section className="hero d-flex justify-content-center align-items-center text-center text-light">
         <div className="hero-overlay w-100 h-100 d-flex justify-content-center align-items-center">
